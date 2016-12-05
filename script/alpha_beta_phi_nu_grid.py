bf6aa5912bb673816df758d444ddcda3d305d4e0
Qiqi Wang <qiqi@mit.edu>
2016-12-04 22:48:27 -0500

    Scripts

--- script/alpha_beta_phi_nu_grid.py
+++ script/alpha_beta_phi_nu_grid.py
@@ -1,6 +1,7 @@
 import os
+import shutil
 import subprocess
-from itertools import product
+import itertools
 from numpy import *
 
 Alpha = array([0.05, 0.1, 0.2])
@@ -8,16 +9,18 @@ Beta = array([0.5, 1.0])
 Phi_over_pi = array([0.001, 1./12, 1./6, 1./4, 1./3])
 Nu = array([0.0002, 0.0003, 0.0005, 0.001])
 
-for alpha, beta, phi_over_pi, nu in product(Alpha, Beta, Phi_over_pi, Nu):
+for alpha, beta, phi_over_pi, nu in itertools.product(Alpha, Beta, Phi_over_pi, Nu):
     alpha = str(alpha)
     beta = str(beta)
     phi_over_pi = str(phi_over_pi)
     nu = str(nu)
     path = '{0}/{1}/{2}/{3}'.format(alpha, beta, phi_over_pi, nu)
-    os.mkdirs(path)
     T, nx, ny, nz = '10', '120', '40', '50'
     if not os.path.exists(os.path.join(path, 'proessor0', T)):
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        os.makedirs(path)
         subprocess.check_call([
-            'python', 'runcase.py',
+            'python', 'runcase.py', path,
             alpha, beta, phi_over_pi,
             nu, T, nx, ny, nz])
